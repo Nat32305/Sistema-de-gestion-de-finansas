38d9832c1a89a2f046feaaad199e0be42f70ed38
Nat32305 <nsanta26@misena.edu.co>
2022-11-27 11:24:53 -0500

    patron de diseño MVC

---
+++ empresa/head.php
@@ -0,0 +1,5 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Sistema de gestion financiera</title>
+<link rel="stylesheet" href="./assets/css/bulma.min.css">
+<link rel="stylesheet" href="./assets/css/estilos.css">
